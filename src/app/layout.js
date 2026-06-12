785fef4b995d6383ab651639de004852f8532b91
Gabriel Angelo <gabriel.dias@rtdatabr.com>
2026-06-12 18:37:00 -0300

    upd

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { StoreProvider } from "@/components/store-context";
 import Header from "@/components/header";
+import { StoreProvider } from "@/components/store-context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
